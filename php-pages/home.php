33de081cb615e293d75e613300972590accec2de
AlexDasi <alejandrodasi@gmail.com>
2022-06-07 12:41:31 +0200

    messages added

--- php-pages/home.php
+++ php-pages/home.php
@@ -1,11 +1,17 @@
 <div id="home">
     <section class="categories">
         <ul>
-            <li id="graphic" class="categories--items text-hover-fill-goes-right"><a class="main-text graphic" href="">GRAPHIC</a></li>
+            <!--graphic-->
+            <li id="graphic" class="categories--items categories--items-graphic text-hover-fill-goes-right"><a class="main-text graphic" href="">GRAPHIC</a></li>
             <li id="message-graphic" class="secondary-text message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus eveniet sequi culpa pariatur, corrupti iure vero impedit temporibus perspiciatis deserunt, placeat voluptate voluptates qui exercitationem, incidunt aliquam totam voluptas.</li>
 
-            <li onmouseover="hideui()" onmouseout="hideui()" class="categories--items"><a class="main-text ui" href="">USER INTERFACE</a></li>
-            <li onmouseover="hideweb()" class="categories--items"><a class="main-text web" href="">WEB</a></li>
+            <!--ui-->
+            <li id="ui" class="categories--items categories--items-ui"><a class="main-text ui" href="">USER INTERFACE</a></li>
+            <li id="message-ui" class="secondary-text message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus eveniet sequi culpa pariatur, corrupti iure vero impedit temporibus perspiciatis deserunt, placeat voluptate voluptates qui exercitationem, incidunt aliquam totam voluptas.</li>
+
+            <!--web-->
+            <li id="web" class="categories--items categories--items-web"><a class="main-text web" href="">WEB</a></li>
+            <li id="message-web" class="secondary-text message">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus eveniet sequi culpa pariatur, corrupti iure vero impedit temporibus perspiciatis deserunt, placeat voluptate voluptates qui exercitationem, incidunt aliquam totam voluptas.</li>
         </ul>
         <p class="main-text categories--designer">designer</p>
     </section>

c25cbe2f8334d98f84d6680eeaf519a5b2efa23f
AlexDasi <alejandrodasi@gmail.com>
2022-06-08 21:42:36 +0200

    spacing fixed

--- php-pages/works wpversion.php
+++ php-pages/works wpversion.php
@@ -3,6 +3,29 @@
     <!-- Swiper -->
     <div class="swiper WorksSwiper">
         <div class="swiper-wrapper">
+
+        <?php
+
+$works = new WP_Query ( array
+(
+    'post_type' => 'post',
+    'posts_per_page' => -1
+));
+
+?>
+
+<?php while ( $works -> have_posts() ) : $works -> the_post(); ?>
+
+<div class="swiper-slide">
+
+
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail ( 'medium' ); ?></a>
+
+
+</div>
+
+<?php endwhile; ?>
+
             <div class="swiper-slide">
                 <img src="images/pictures/camisola.jpg"/>
             </div>

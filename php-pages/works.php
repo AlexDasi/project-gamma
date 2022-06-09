106d0094481d29ff2ed8de2099e797411b928051
AlexDasi <alejandrodasi@gmail.com>
2022-06-09 21:24:42 +0200

    works swiper - faltan ajustes
    solo falta arreglar las animaciones y tamaños

--- php-pages/works.php
+++ php-pages/works.php
@@ -5,9 +5,25 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide works works-1">
                 <img src="images/pictures/camisola.jpg"/>
+                <div class="works--info">
+                    <h2 >CAMISOLA</h2>
+                    <p class="yellow-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, minima quidem, distinctio nobis magnam repudiandae consectetur perferendis quia dolores consequuntur beatae esse officia ut sed molestias? Aperiam non dicta id!</p>
+                </div>
+                <div class="works--details">
+                    <p>Branding and web design</p>
+                    <p class="yellow-600">2022</p>
+                </div>
             </div>
-            <div class="swiper-slide">
+            <div class="swiper-slide works works-2">
                 <img src="images/pictures/Terralava.jpg"/>
+                <div class="works--info">
+                    <h2 >TERRALAVA</h2>
+                    <p class="yellow-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, minima quidem, distinctio nobis magnam repudiandae consectetur perferendis quia dolores consequuntur beatae esse officia ut sed molestias? Aperiam non dicta id!</p>
+                </div>
+                <div class="works--details">
+                    <p>Co-founder, branding, web design and marketing</p>
+                    <p class="yellow-600">2021</p>
+                </div>
             </div>
             <div class="swiper-slide">
                 <img src="images/pictures/ROTCPop-up.jpg"/>
@@ -34,7 +50,8 @@
                 <img src="images/pictures/burbuja-wood-banner.jpg"/>
             </div>
         </div>
-        <div class="swiper-pagination"></div>
 
+        <!-- pagination -->
+        <div class="swiper-pagination"></div>
     </div>
 </div>

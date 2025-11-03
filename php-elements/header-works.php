8f07883388aed00bfd92d7a33774c3b9d6fc849a
AlexDasi <alejandrodasi@gmail.com>
2025-11-03 16:47:36 +0100

    fix(works): enable custom cursor on project detail pages

--- php-elements/header-works.php
+++ php-elements/header-works.php
@@ -22,8 +22,8 @@
 
     <!-- cursor -->
 
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="../js/cursor.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="../../js/cursor.js"></script>
 
 
 

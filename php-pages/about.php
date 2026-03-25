998a89651c12e53103c15c711a96ccd45c26f82a
AlexDasi <alejandrodasi@gmail.com>
2026-03-25 20:08:28 +0100

    chore(cv): replace resume file and update download link

--- php-pages/about.php
+++ php-pages/about.php
@@ -9,7 +9,7 @@
                 <div class="slice-infos__text"><p>For over a decade, I've been shaping ideas through design — first as a freelancer, then collaborating with different companies. Over time, I've learned that great visual communication isn't just about aesthetics, but about creating meaning and connection. Recently, I expanded my toolkit with a postgraduate degree in Web Development, UX/UI, and Marketing, blending artistry with strategy. As the ways we communicate evolve every day, I believe the key is to stay curious, adapt fast, and never stop learning.</p></div>
             
                 <div class="cv-download-container">
-                    <a href="/content/downloadables/Resume Alex Dasi 2025.pdf" class="cv-download-btn effect magnet" download>DOWNLOAD CV</a>
+                    <a href="/content/downloadables/Resume Alex Dasi 2026.pdf" class="cv-download-btn effect magnet" download>DOWNLOAD CV</a>
                 </div>
             </div>
         </section>
